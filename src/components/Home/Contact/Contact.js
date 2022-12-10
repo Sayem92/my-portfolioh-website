3a5aabc2fb3ccbf57ff07843e01500fb52ee612c
Sayem92 <sayemmiha123@gmail.com>
2022-12-10 22:52:29 +0600

    update contact form

--- src/components/Home/Contact/Contact.js
+++ src/components/Home/Contact/Contact.js
@@ -4,14 +4,17 @@ import emailjs from '@emailjs/browser';
 import { toast } from 'react-hot-toast';
 
 const Contact = () => {
-
-
     const form = useRef();
+
+    const service_Id = process.env.REACT_APP_SERVICE_EMAIL_ID
+    const template_Id = process.env.REACT_APP_TEMPLATE_ID;
+    const public_Key = process.env.REACT_APP_PUBLIC_KEY;
+ 
 
     const sendEmail = (e) => {
         e.preventDefault();
 
-        emailjs.sendForm(process.env.REACT_APP_SERVICE_EMAIL_ID, process.env.REACT_APP_TEMPLATE_ID, form.current, process.env.REACT_APP_PUBLIC_KEY)
+        emailjs.sendForm(service_Id, template_Id, form.current, public_Key)
             .then((result) => {
                 console.log(result.text);
                 toast.success("send message");
@@ -41,7 +44,7 @@ const Contact = () => {
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                                 </svg>
 
-                                <span className="mx-2 truncate w-72 text-gray-200">
+                                <span className="mx-2 truncate md:w-72 text-gray-200">
                                     Moulvibazar, Bangladesh
                                 </span>
                             </p>
@@ -51,7 +54,7 @@ const Contact = () => {
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                                 </svg>
 
-                                <span className="mx-2 truncate w-72 text-gray-200">01748-433092</span>
+                                <span className="mx-2 truncate md:w-72 text-gray-200">01748-433092</span>
                             </p>
 
                             <p className="flex items-start -mx-2">
@@ -59,12 +62,12 @@ const Contact = () => {
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                                 </svg>
 
-                                <span className="mx-2  truncate w-72 text-gray-200">sayemmiha123@gmail.com</span>
+                                <span className="mx-2  truncate md:w-72 text-gray-200">sayemmiha123@gmail.com</span>
                             </p>
                         </div>
 
-                        <div className="mt-6 w-80 md:mt-8">
-                            <h3 className="text-gray-200 ">Follow me</h3>
+                        <div className="mt-6 md:w-80 md:mt-8">
+                            <h3 className="text-gray-200">Follow me</h3>
 
                             <div className="flex mt-4 -mx-1.5 ">
 
