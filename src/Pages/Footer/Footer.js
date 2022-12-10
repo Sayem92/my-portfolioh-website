1d2471e098a7437b9b63c422add7dfa134d3f23d
Sayem92 <sayemmiha123@gmail.com>
2022-12-10 17:51:23 +0600

    add footer

---
+++ src/Pages/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer footer-center p-10 text-gray-200">
+            <div>
+                <p>Copyright © 2022 - All right reserved by MD. Sayem Miah</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Pages/Footer/Footer';
 import Navbar from '../Pages/Navbar/Navbar';
 
 const Main = () => {
@@ -7,6 +8,7 @@ const Main = () => {
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
